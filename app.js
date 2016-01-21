81fd386e4e4b96250883257c66487132361c34cf
Frances Harlow <frances.harlow@gmail.com>
2016-01-21 17:28:26 -0500

    incremental sequelize progress

--- app.js
+++ app.js
@@ -1,32 +1,24 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 var bodyParser = require('body-parser');
-var ejs = 'ejs';
+var cookieParser = require('cookie-parser');
+var ejs = require('ejs');
 var Sequelize = require('sequelize');
 
 // Configuration
+app.user(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/bower_components'));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-//Database
-// sequelize initialization
-var sequelize = new Sequelize("postgres://username:password@localhost:5432/dbname");
- 
-// check database connection
-sequelize.authenticate().complete(function(err) {
-    if (err) {
-      console.log('Unable to connect to the database:', err);
-    } else {
-      console.log('Connection has been established successfully.');
-    }
-});
+var routes = require('./routes/index');
+var users  = require('./routes/users');
 
-// Routes
-app.get('/', function(req, res){
-  res.render('index');
-})
+app.use('/', routes);
+app.use('/users', users);
 
-app.listen(process.env.PORT || 3000);
+module.exports = app;
