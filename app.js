dc65c0d87f4d51ccc62d9cf4b8bcec6d97c9e539
Frances Harlow <frances.harlow@gmail.com>
2016-01-22 13:29:45 -0500

    sequelize success! (thanks Sung)

--- app.js
+++ app.js
@@ -1,32 +1,47 @@
 var express = require('express');
-var app = express();
+var path = require('path');
 var bodyParser = require('body-parser');
-var ejs = 'ejs';
+var ejs = require('ejs');
+var cookieParser = require('cookie-parser')
+// var session = require('express-sessions');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// db
 var Sequelize = require('sequelize');
 
-// Configuration
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/bower_components'));
-app.set('view engine', 'ejs')
+var sequelize = new Sequelize('postgres://localhost:5432/hej_hej_app')
+console.log(sequelize);
 
-//Database
-// var sequelize = new Sequelize('database', 'username', 'password', {
-//   host: 'localhost',
-//   dialect: 'mysql'|'mariadb'|'sqlite'|'postgres'|'mssql',
+sequelize
+  .authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully.');
+  }, function (err) { 
+    console.log('Unable to connect to the database:', err);
+  });
 
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     idle: 10000
-//   },
+// var SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-//   // SQLite only
-//   storage: 'path/to/database.sqlite'
-// });
+app.use(cookieParser())
+// app.use(session({
+//   secret: 'keyboard cat',
+//   store: new SequelizeStore({
+//     db: sequelize
+//   }),
+//   proxy: true // if you do SSL outside of node.
+// }))
 
-// Routes
+//routes
 app.get('/', function(req, res){
   res.render('index');
 })
